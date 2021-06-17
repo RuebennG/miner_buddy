d1abfc0d916cf654e44e1120f3b1696054370cce
RuebennG <55192052+RuebennG@users.noreply.github.com>
2021-06-17 22:55:21 +0800

    Update update_gsheets.py

--- update_gsheets.py
+++ update_gsheets.py
@@ -1,7 +1,7 @@
 from nanopool.api_calls import eth_nanopool
 from gsheets.api_calls import gsheets_api
 
-wallet = '0x56d890f48b0ce7860d5511432a970c47e7bcdf53'
+wallet = '' #insert wallet
 
 updates = eth_nanopool(wallet)\
                 .details().values.tolist()
